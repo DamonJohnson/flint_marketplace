8606751348b01e521f2b76af7ee704d0bbd20713
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-05-18 19:56:17 +1000

    removed unused migrations

--- db/migrate/20220512042558_remove_profile_from_user.rb
+++
@@ -1,7 +0,0 @@
-class RemoveProfileFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :first_name
-    remove_column :users, :last_name
-    remove_column :users, :location_id
-  end
-end
--- db/migrate/20220512111618_remove_location_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLocationFromItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :items, :location_id
-  end
-end
